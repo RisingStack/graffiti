4349a482d9364b50f9dfc964236bdee36ed26c3e
Gergely Nemeth <mail@nemethgergely.com>
2015-07-25 19:01:32 +0200

    test(express): add failing test cases for Express middleware

--- src/express/index.spec.js
+++ src/express/index.spec.js
@@ -4,4 +4,22 @@ describe('graffiti express', function() {
 
   var express = require('./');
 
+  it('checks for required options');
+
+  it('creates the schema');
+
+  describe('requested url starts with prefix', function() {
+
+    it('returns with proper results');
+
+    it('returns with an error');
+
+  });
+
+  describe('requested url does not start with prefix', function() {
+
+    it('calls the next middleware');
+
+  });
+
 });
