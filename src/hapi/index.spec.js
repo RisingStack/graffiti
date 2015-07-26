1be58adae5e2566315bf3b970f192f2ada366037
Gergely Nemeth <mail@nemethgergely.com>
2015-07-26 18:08:21 +0200

    test(hapi): improve Hapi plugin test coverage

--- src/hapi/index.spec.js
+++ src/hapi/index.spec.js
@@ -1,4 +1,5 @@
 var expect = require('chai').expect;
+var Hapi = require('hapi');
 
 describe('graffiti hapi', function() {
 
@@ -23,6 +24,47 @@ describe('graffiti hapi', function() {
 
       throw new Error('Error should have been thrown');
 
+    });
+
+  });
+
+  describe('returns with proper results', function(done) {
+
+    var server = new Hapi.Server();
+    server.connection({ port: 3000 });
+
+    var result = {
+      users: [1, 2, 3]
+    };
+
+    server.register({
+      register: hapi.create({
+        graphql: function() {
+          return Promise.resolve(result);
+        }
+      }),
+      options: {
+        models: [],
+        adapter: {
+          getSchema: function() {}
+        }
+      }
+    }, {
+      routes: {
+        prefix: '/graphql'
+      }
+    }, function(err) {
+      if (err) {
+        return done(err);
+      }
+
+      server.inject({
+        method: 'GET',
+        url: '/graphql?q={__type}'
+      }, function(res) {
+        expect(JSON.parse(res.payload)).to.eql(result);
+        done();
+      });
     });
 
   });
